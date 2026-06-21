bb8ee7369921de144641d72213b8db65f5f15550
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-22 00:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
+    let f = document.getElementById('fname').value
+    let l = document.getElementById('lname').value
 
-}
+	alert(`${f} ${l}`);
+};
